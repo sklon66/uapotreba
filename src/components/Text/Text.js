42b3ca190d56c7cf7371eabfd2ec1c0c47d678a3
Kirill Osadchiy <kirill.osadchiy@wikrgroup.com>
2022-03-07 21:44:36 +0200

    test commit

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -6,7 +6,7 @@ import { KEYS_UA } from "../../locales/translationUa";
 function Text({text='criticality', language}) {
 
     const translated = language === 'ua' ? KEYS_UA[text] : KEYS_EN[text]
-
+    console.log('try to catch me run dirty')
     return <p>{translated}</p>;
 }
 
