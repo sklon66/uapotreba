ea097f1c532d903b2c0abc749dcf75efeb71ed24
vladyslav chernenko <vladyslav.chernenko@wikrgroup.com>
2022-03-11 17:00:01 +0200

    update API link

--- src/api/apiData.js
+++ src/api/apiData.js
@@ -2,7 +2,7 @@ import { setData, setProducts } from "../redux/AppReducer/actions.js";
 
 export const getData = (dispatch) => {
 
-    fetch(`https://script.google.com/macros/s/AKfycbyoxZ-GJjWpxFpw3ixGrI2upjMJyzK7HEcsOyBMVEvTPT8JY-IvPaD0bFqS7aqYQ14J/exec`)
+    fetch(`https://script.google.com/macros/s/AKfycbyCJlPIWMJD6tiTK2P6zaeheulvi6mHnjnSra3Gyz0txxrpUEVK5hINaPe3uf840Jnu/exec`)
         .then(function (response) { return response.json(); })
         .then(function (data) {
             if (data) {
