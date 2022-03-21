ff32c0449d1d32bb49c0dac5314b8809b16aece0
Kirill Osadchiy <protocolisimo@gmail.com>
2022-03-21 11:38:36 +0200

    Fix mobile filter issue with 'all category'

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -3,13 +3,14 @@ import React from "react";
 // styles
 import Text from '../../components/Text'
 
-const Checkbox = ({ value, handleChange, key }) => {
+const Checkbox = ({ value, handleChange, key, checked }) => {
     return (
         <label key={key}>
             <input
                 name='product'
                 value={value}
                 type="radio"
+                // checked={checked}
                 onChange={handleChange}
             />
             <Text text={value} />
